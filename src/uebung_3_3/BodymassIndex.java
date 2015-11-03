11df91a017d1b4df2dfda3d7aebd413d344d01a1
MankeT <tim.manke@nordakademie.de>
2015-11-03 09:48:03 +0100

    Aktualisierung und Verbesserung.

--- src/uebung_3_3/BodymassIndex.java
+++ src/uebung_3_3/BodymassIndex.java
@@ -10,58 +10,58 @@ public class BodymassIndex {
 	
 	double bmi = gewicht/(groesse*groesse);
 	
-	if (bmi < 20 && maennlich == true && alter < 25)
+	if (bmi < 20 && maennlich && alter < 25)
 	{System.out.println("Du bist zu dünn!");}
-	else {if (bmi > 25 && maennlich == true && alter < 25)
+	else {if (bmi > 25 && maennlich && alter < 25)
 	{System.out.println("Du bist zu dick!");}
-	else {if (bmi < 19 && maennlich == false && alter < 25)
+	else {if (bmi < 19 && !maennlich && alter < 25)
 	{System.out.println("Du bist zu dünn!");}
-	else {if (bmi > 24 && maennlich == false && alter < 25)
-	{System.out.println("Du bist zu dick!");}
-	
-	else {if (bmi < 21 && maennlich == true && alter < 35)
-	{System.out.println("Du bist zu dünn!");}
-	else {if (bmi > 26 && maennlich == true && alter < 35)
-	{System.out.println("Du bist zu dick!");}
-	else {if (bmi < 20 && maennlich == false && alter < 35)
-	{System.out.println("Du bist zu dünn!");}
-	else {if (bmi > 25 && maennlich == false && alter < 35)
+	else {if (bmi > 24 && !maennlich && alter < 25)
 	{System.out.println("Du bist zu dick!");}
 	
-	else {if (bmi < 22 && maennlich == true && alter < 45)
+	else {if (bmi < 21 && maennlich && alter < 35)
 	{System.out.println("Du bist zu dünn!");}
-	else {if (bmi > 27 && maennlich == true && alter < 45)
+	else {if (bmi > 26 && maennlich && alter < 35)
 	{System.out.println("Du bist zu dick!");}
-	else {if (bmi < 21 && maennlich == false && alter < 45)
+	else {if (bmi < 20 && !maennlich && alter < 35)
 	{System.out.println("Du bist zu dünn!");}
-	else {if (bmi > 26 && maennlich == false && alter < 45)
-	{System.out.println("Du bist zu dick!");}
-	
-	else {if (bmi < 23 && maennlich == true && alter < 55)
-	{System.out.println("Du bist zu dünn!");}
-	else {if (bmi > 28 && maennlich == true && alter < 55)
-	{System.out.println("Du bist zu dick!");}
-	else {if (bmi < 22 && maennlich == false && alter < 55)
-	{System.out.println("Du bist zu dünn!");}
-	else {if (bmi > 27 && maennlich == false && alter < 55)
+	else {if (bmi > 25 && !maennlich && alter < 35)
 	{System.out.println("Du bist zu dick!");}
 	
-	else {if (bmi < 24 && maennlich == true && alter < 65)
+	else {if (bmi < 22 && maennlich && alter < 45)
 	{System.out.println("Du bist zu dünn!");}
-	else {if (bmi > 29 && maennlich == true && alter < 65)
+	else {if (bmi > 27 && maennlich && alter < 45)
 	{System.out.println("Du bist zu dick!");}
-	else {if (bmi < 23 && maennlich == false && alter < 65)
+	else {if (bmi < 21 && !maennlich && alter < 45)
 	{System.out.println("Du bist zu dünn!");}
-	else {if (bmi > 28 && maennlich == false && alter < 65)
+	else {if (bmi > 26 && !maennlich && alter < 45)
 	{System.out.println("Du bist zu dick!");}
 	
-	else {if (bmi < 25 && maennlich == true && alter > 64)
+	else {if (bmi < 23 && maennlich && alter < 55)
 	{System.out.println("Du bist zu dünn!");}
-	else {if (bmi > 30 && maennlich == true && alter > 64)
+	else {if (bmi > 28 && maennlich && alter < 55)
 	{System.out.println("Du bist zu dick!");}
-	else {if (bmi < 24 && maennlich == false && alter > 64)
+	else {if (bmi < 22 && !maennlich && alter < 55)
 	{System.out.println("Du bist zu dünn!");}
-	else {if (bmi > 29 && maennlich == false && alter > 64)
+	else {if (bmi > 27 && !maennlich && alter < 55)
+	{System.out.println("Du bist zu dick!");}
+	
+	else {if (bmi < 24 && maennlich && alter < 65)
+	{System.out.println("Du bist zu dünn!");}
+	else {if (bmi > 29 && maennlich && alter < 65)
+	{System.out.println("Du bist zu dick!");}
+	else {if (bmi < 23 && !maennlich && alter < 65)
+	{System.out.println("Du bist zu dünn!");}
+	else {if (bmi > 28 && !maennlich && alter < 65)
+	{System.out.println("Du bist zu dick!");}
+	
+	else {if (bmi < 25 && maennlich && alter > 64)
+	{System.out.println("Du bist zu dünn!");}
+	else {if (bmi > 30 && maennlich && alter > 64)
+	{System.out.println("Du bist zu dick!");}
+	else {if (bmi < 24 && !maennlich && alter > 64)
+	{System.out.println("Du bist zu dünn!");}
+	else {if (bmi > 29 && !maennlich && alter > 64)
 	{System.out.println("Du bist zu dick!");}
 	else {System.out.println("Dein BMI ist im normalen Bereich");}}}}}}}}}}}}}}}}}}}}}}}}
 	
